d30dc844439755fce4cd8e138e32be1c7f4ce0c0
Aniket Prakash <b218004@iiit-bh.ac.in>
2022-04-18 12:43:39 +0530

    chore: add chakra-ui

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { ChakraProvider } from '@chakra-ui/react'
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
